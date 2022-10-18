a5e1681e8bc5047aee6dfc7a20774dab32dc6f10
Marcus <marcusvinicius.767.mv@gmail.com>
2022-10-18 19:57:44 -0300

    Criado primeiros models e usuario admin

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'core/layouts/index.html')
+
+
+def contato(request):
+    return render(request, 'core/layouts/contato.html')
