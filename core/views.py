6ac062a20f080d7a5093dcda593240883c3bf50e
Marcus <marcusvinicius.767.mv@gmail.com>
2022-10-25 22:36:50 -0300

    finalizando projeto base

--- core/views.py
+++ core/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
+from .models import Produto
 
 
 def index(request):
-    return render(request, 'core/layouts/index.html')
+    produtos = Produto.objects.all()
+
+    contexto = {
+        'produtos': produtos
+    }
+    return render(request, 'core/layouts/index.html', contexto)
 
 
 def contato(request):
     return render(request, 'core/layouts/contato.html')
+
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+    context = {
+        'produto': prod
+    }
+    return render(request, 'core/layouts/produto.html', context)
